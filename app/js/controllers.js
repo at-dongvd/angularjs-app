139f0b676b15a6821da01537721e50af65b182c5
at-dongvd <vddongbk@gmail.com>
2016-07-03 15:20:50 +0700

    use http method receive data for information and contact

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,37 +4,29 @@ var templateControllers = angular.module('templateControllers', [
   'ngRoute'
 ]);
 
-templateControllers.controller('AboutController', ['$scope', '$route',
-  function ($scope, $route) {
-    // Controller method for Home
-    $scope.person = {
-    	fullname: 'Võ Doãn Đồng',
-    	birthday: '28/10/1994',
-    	gender: 'Male',
-    	nativeland: 'Quang Binh',
-    	job: 'Intership',
-    	team: 'Team Php',
-    	email: 'vddongbk@gmail.com',
-    	phone: '(+84) 165 747 4245',
-    	github: 'at-dongvd',
-    	urlgit: 'https://github.com/at-dongvd',
-    	urlcv: 'https://i.topcv.vn/dongvodoan?ref=126024',
-    	html: '70%',
-    	js: '60%',
-    	design: '65%',
-    	english: '70%',
-    	php: '80%',
-    };
+templateControllers.controller('AboutController', ['$scope', '$http',
+  function ($scope, $http) {
+    // Controller method for About
+    var url = 'http://localhost:8000/data/person.json';   
+  $http.get(url).success(function(response) {
+    $scope.person = response;
+  });
 }]);
 
 templateControllers.controller('AppController', ['$scope', '$route',
   function ($scope, $route) {
-    // Controller method for About
-    $scope.template = $route.current.templateUrl;
-}]);
-templateApp.controller('HeaderPartialCtrl', ['$scope', function ($scope) {
+    // Controller method for App
+  
 }]);
 
-templateApp.controller('FooterPartialCtrl', ['$scope', function ($scope) {
-  // Do something...
+templateApp.controller('HeaderPartialCtrl', ['$scope', function ($scope) {
+    //Do something
+}]);
+
+templateApp.controller('FooterPartialCtrl', ['$scope', '$http', function ($scope, $http) {
+  var url = 'http://localhost:8000/data/contact.json';
+   $http.get(url).success( function(response) {
+                           $scope.contact = response.contact;
+                           $scope.icon = response.icon;                     
+    });
 }]);
