1daafcf7e9d90cb2f1b9217f86ca30b9da90fd42
at-dongvd <vddongbk@gmail.com>
2016-07-03 22:39:08 +0700

    built showcase page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,10 +13,22 @@ templateControllers.controller('AboutController', ['$scope', '$http',
   });
 }]);
 
-templateControllers.controller('AppController', ['$scope', '$route',
-  function ($scope, $route) {
+templateControllers.controller('AppController', ['$scope', '$http',
+  function ($scope, $http) {
     // Controller method for App
-  
+  var url = 'http://localhost:8000/data/rate.json';   
+  $http.get(url).then(function(response) {
+    $scope.rates= response.data.rates;
+    // $scope.toType= $scope.rates.VND;
+    // $scope.fromType = $scope.rates.VND;
+    $scope.fromValue =1;
+    $scope.forExConvert();
+  });
+
+ $scope.forExConvert = function() {
+   $scope.toValue = $scope.fromValue * ($scope.toType * (1 / $scope.fromType));
+   $scope.toValue = $scope.toValue;
+ };
 }]);
 
 templateApp.controller('HeaderPartialCtrl', ['$scope', function ($scope) {
